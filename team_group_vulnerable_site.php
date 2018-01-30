064d84b5a84bdea58258192fa860413fc7c6df36
Unknown <2002kbohlander@gmail.com>
2018-01-29 18:19:52 -0800

    First Commit!

---
+++ team_group_vulnerable_site.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+		<title>Team Group Website</title>
+        <style>p.indent{ padding-left: 3em }</style>
+	</head>
+	<body>
+		<form action="" method="post">
+            <p class="indent">
+            <br /><br />
+			First Name<br />
+            <input type="text" name="firstName" /><br /><br /><br />
+            Last Name<br />
+            <input type="text" name="lastName" /><br /><br /><br />
+            Street Address<br />
+            <input type="text" name="streetAddress" /><br /><br /><br />
+            City<br />
+            <input type="text" name="city" /><br /><br /><br />
+            State<br />
+            <input type="text" name="state" /><br /><br /><br />
+            Zip<br />
+            <input type="text" name="zip" /><br /><br /><br />
+            Username<br />
+            <input type="text" name="username" /><br /><br /><br />
+			Password<br />
+            <input type="password" name="password"><br /><br /><br />
+			<input type="submit" value="Submit" />
+		</form>
+	</body>
+</html>
