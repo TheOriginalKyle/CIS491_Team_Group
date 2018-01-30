f0db35068fdfdefca12978a2516a41d8504917f8
TheOriginalKyle <2002kbohlander@gmail.com>
2018-01-29 19:10:04 -0800

    Redesigned form

--- team_group_vulnerable_site.php
+++ team_group_vulnerable_site.php
@@ -1,7 +1,6 @@
 <html>
 	<head>
 		<title>Team Group Website</title>
-        <style>p.indent{ padding-left: 3em }</style>
 				<meta charset="utf-8">
 			  <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -9,30 +8,40 @@
 			  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 	</head>
 	<body>
+		<div class="jumbotron">
+			<div class="container">
 		<form action="" method="post">
-            <p class="indent">
-            <br /><br />
-			First Name<br />
-            <input type="text" name="firstName" /><br /><br /><br />
-            Last Name<br />
-            <input type="text" name="lastName" /><br /><br /><br />
-            Street Address<br />
-            <input type="text" name="streetAddress" /><br /><br /><br />
-            City<br />
-            <input type="text" name="city" /><br /><br /><br />
-            State<br />
-            <input type="text" name="state" /><br /><br /><br />
-            Zip<br />
-            <input type="text" name="zip" /><br /><br /><br />
-            Username<br />
-            <input type="text" name="username" /><br /><br /><br />
-			Password<br />
-            <input type="password" name="password"><br /><br /><br />
-			<input type="submit" value="Submit" />
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input type="text" id="username" class="form-control" placeholder="fluffyWubkins42" style="width:250px">
+			</div>
+			<div class="form-group">
+				<label for="firstName">First Name</label>
+				<input type="text" id="firstName" class="form-control" placeholder="Ted" style="width:250px">
+			</div>
+			<div class="form-group">
+				<label for="lastName">Last Name</label>
+				<input type="text" id="lastName" class="form-control" placeholder="Mosby" style="width:250px">
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" id="password" class="form-control" placeholder="***********" style="width:250px">
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="sanatizeCheck">
+  			<label class="form-check-label" for="sanatizeCheck">Sanatize Inputs</label>
+			</div>
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="postCheck">
+  			<label class="form-check-label" for="postCheck">Make it a Post Request</label>
+			</div>
+		</div>
+			<button type="submit" class="btn btn-primary" id="submitBtn">Submit</button>
 		</form>
-		<script>
-			//JavaScript here
-		</script>
+	</div>
+		<?php
+
+		 ?>
 	</body>
 
 	<!-- Dependencies at bottom for faster load time -->
@@ -41,5 +50,5 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-	
+
 </html>
